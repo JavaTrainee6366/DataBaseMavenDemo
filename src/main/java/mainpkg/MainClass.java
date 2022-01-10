55f6080af383d8ebb3e49e8e0334646834980e3f
JavaPractice <piyugupta0786@gmail.com>
2022-01-10 11:03:22 +0530

    commit(10/1/2022)

--- src/main/java/mainpkg/MainClass.java
+++ src/main/java/mainpkg/MainClass.java
@@ -7,6 +7,8 @@ public class MainClass {
 	public static void main(String[] args) {
 		
 		JDBC_Connection.db();
+		DBSample obj = new DBSample();
+		obj.insert("Rama", "657362627", "rama@abc.com", "123");
 				
 	}
 
